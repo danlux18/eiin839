44523011b16783b47c4907a5a6337fa858944f92
dan18 <dan.nakache@etu.univ-cotedazur.fr>
2022-03-15 15:42:08 +0100

    TD6

---
+++ TD6/Math_SOAP_Web_Service_Client/Math_SOAP_Web_Service_Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_SOAP_Web_Service_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MathServiceReference.MathsOperationsClient();
+            Console.WriteLine(client.Add(1, 45));
+        }
+    }
+}
